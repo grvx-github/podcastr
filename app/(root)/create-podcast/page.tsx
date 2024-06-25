e8fde0b8be877fa9c39431d77aec33cd72cb538f
grvx <spamgauravjha@gmail.com>
2024-06-25 17:20:14 +0530

    create-podcast rerouted

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -3,12 +3,15 @@ import PodcastForm from "@/components/podcastForm"
 import { authConfig } from "@/lib/auth"
 import { getServerSession } from "next-auth"
 import { signIn } from "next-auth/react" // Import the signIn function from next-auth/react
+import { redirect } from "next/navigation"
 
 export default async function CreatePodcast() {
   const session = await getServerSession(authConfig)
 
   if (!session) {
-    signIn("google") // Initiate sign-in with Google
+    // If there's no session, redirect to sign-in page
+    redirect("/api/auth/signin?callbackUrl=/create-podcast")
+    return null // Ensure nothing is rendered
   }
 
   return (
